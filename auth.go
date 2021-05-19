09bb8809cca0a61fa9b929a3234c4b3f627fecdf
LakshyaNegi <lakshya.negi.1997@gmail.com>
2021-05-19 20:13:54 +0530

    cookie login and auth added

--- auth.go
+++ auth.go
@@ -37,6 +37,15 @@ func MustAuth(handler http.Handler) http.Handler {
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if c, err := r.Cookie("auth"); err == http.ErrNoCookie {
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		c.Name = "Deleted"
+		c.Value = "Unuse"
+		c.Expires = time.Now()
+	}
+
 	if r.Method != http.MethodPost {
 		log.Printf("Login method was not post")
 		http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -59,9 +68,10 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
 			w.Header()["Location"] = []string{"/chat"}
 			w.WriteHeader(http.StatusTemporaryRedirect)
+			return
 		}
 	}
 
 	log.Printf("User not found")
-
+	return
 }
